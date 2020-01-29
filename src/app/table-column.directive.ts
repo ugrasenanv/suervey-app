72f0e3df25ce4b4f7e4c4716e75c2e96e584d2aa
ugrasenanv <ugrasenanv@gmail.com>
2020-01-29 21:39:30 +0530

    1 file modified

--- src/app/table-column.directive.ts
+++ src/app/table-column.directive.ts
@@ -1,6 +1,6 @@
 import {ContentChild, Directive, Input, OnInit} from '@angular/core';
-import {CellCallback} from '../tools/types';
-import {DataTableRow} from '../row/row.component';
+// import {CellCallback} from '../tools/types';
+// import {DataTableRow} from '../row/row.component';
 
 @Directive({
   selector: '[appTableColumn]'
