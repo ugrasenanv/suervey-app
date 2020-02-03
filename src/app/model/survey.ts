7b47ab20ebf6d6bf0db78651e9107f2ac935ccb4
ugrasenanv <ugrasenanv@gmail.com>
2020-02-03 21:12:17 +0530

    added survey store and action

---
+++ src/app/model/survey.ts
@@ -0,0 +1,11 @@
+export interface Survey {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  gender?: string;
+  phoneNumber?: number;
+  preferences?: string;
+  rating?: number;
+  date?: string;
+}
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -2,6 +2,10 @@ import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
 import { UsersActions } from '../actions/users.actions';
+
+import { Survey } from '../model/survey';
+import { SurveyActions } from '../actions/survey.actions';
+
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -19,15 +23,17 @@ import { MustMatch } from '../_helpers/must-match.validator';
 export class SurveyComponent implements OnInit {
   registerForm: FormGroup;
     submitted = false;
-   
     loading = false;
     users: User[] = [];
+    surveys: Survey[] = [];
     @select('users') public users$: Observable<User>;
+    @select('surveys') public surveys$: Observable<Survey>;
+
 
 //  constructor(private formBuilder: FormBuilder, private fb: FormBuilder, public actions:  UsersActions) { }
 
-  // constructor(public actions:  UsersActions) {}
-    constructor(private formBuilder: FormBuilder, public actions:  UsersActions) {
+  // constructor(public actions:  UsersActions) {} public actions:  UsersActions,
+    constructor(private formBuilder: FormBuilder, public actions:  UsersActions,) {
     // this.surveyForm = this.fb.group({
     //   firstName: this.fb.control('', Validators.required),
     //   lastName: this.fb.control('', Validators.required)
@@ -37,9 +43,15 @@ export class SurveyComponent implements OnInit {
   surveyForm: FormGroup;
 
   addUser(labelInput: HTMLInputElement) {
+    console.log('labelInput=====>',labelInput);
       this.actions.add(labelInput.value);
       labelInput.value = '';
   }
+
+  // addSurvey(labelInput: HTMLInputElement) {
+  //     this.actions.add(labelInput.value);
+  //     labelInput.value = '';
+  // }
 
     // constructor(private userService: UserService) { }
 
